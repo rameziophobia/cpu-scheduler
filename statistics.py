b74dc3df74fa4dac27c5cfd1ce73ccb0ecffa365
Ramez <rameznoshy@gmail.com>
2019-11-25 04:44:41 +0200

    fix statistics

--- statistics.py
+++ statistics.py
@@ -3,10 +3,10 @@ class Statistics:
         self.turnaround = 0
         self.response_time = -1
         self.wait = 0
-    #     todo throughput
+
+    def calculate_wait(self, final, arrival, burst):
+        self.wait = final - arrival - burst
 
     def calculate_turnaround(self, final, arrival):
-        self.turnaround = final - arrival - self.wait
+        self.turnaround = final - arrival
 
-    def increment_wait(self):
-        self.wait += 1
